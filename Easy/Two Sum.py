a6a5a014a46f6f270c47d8babcc835823a477eae
Kristy Xu <xuwenxiao1212@gmail.com>
2017-10-04 17:49:01 -0400

    Create Two Sum.py

---
+++ Easy/Two Sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)-i-1):
+                if nums[i] + nums[j+i+1] == target:
+                    return [i, j+i+1]
+                else:
+                    continue
+        return []
