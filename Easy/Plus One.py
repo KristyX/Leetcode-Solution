a24b65388dee9a2cf2d2fe941879806bcedb12c5
Kristy Xu <xuwenxiao1212@gmail.com>
2017-10-05 18:51:37 -0400

    Create Plus One.py

---
+++ Easy/Plus One.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = int(''.join(map(str, digits)))
+        return map(int, list(str(num+1)))
